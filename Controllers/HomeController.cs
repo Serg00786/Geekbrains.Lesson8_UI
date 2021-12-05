99b837758df20773c73a56cb19e08f3de9c1b80b
Serg <a_s_v86@mail.ru>
2021-12-05 22:32:55 +0300

    initial

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using GeekBrains.Lesson8.Models;
+using GeekBrains.Lesson8.Data;
+using Autofac;
+
+namespace GeekBrains.Lesson8.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        /// <summary>
+        /// Index
+        /// </summary>
+        /// <returns></returns>
+        public async Task<IActionResult> Index()
+        {
+            
+            var builder = new ContainerBuilder();
+            builder.RegisterType<Repository>().As<IRepository>();
+            IContainer container = builder.Build();
+            IRepository service = container.Resolve<IRepository>();
+
+            var Result = await service.GetStudents();
+            var ResultMapped = Mapping.Mapper.Map<List<ListOfStudent>>(Result);
+            return View(ResultMapped);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/ListOfStudent.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GeekBrains.Lesson8
+{
+    public class ListOfStudent
+    {
+        public int Id { get; set; }
+        public string Names { get; set; }
+        public string Surname { get; set; }
+        public int Age { get; set; }
+    }
+}
